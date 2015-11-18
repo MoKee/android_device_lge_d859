ea68ad5c06179936e324116aca61cdc87bc009b5
qqzwc <qqzwcgo@gmail.com>
2015-11-18 20:34:30 +0800

    device-common:Move common stuff to g3duo-common

--- device.mk
+++ device.mk
@@ -53,4 +53,4 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 # common g3
-$(call inherit-product, device/lge/g3-common/g3.mk)
+$(call inherit-product, device/lge/g3duo-common/g3.mk)
